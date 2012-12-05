62b16bae8e5bc26aacef5c0db484c65a554162b3
xukun <xukun@xukun-asus>
2012-12-05 23:17:40 +0800

    xxx

--- blueprint2/blueprint2-swt-test/src/test/bp2/swt/Test1_helloworld.java
+++ blueprint2/blueprint2-swt-test/src/test/bp2/swt/Test1_helloworld.java
@@ -1,7 +1,5 @@
 package test.bp2.swt;
 
-import java.io.IOException;
-
 import ananas.lib.blueprint2.Blueprint2;
 import ananas.lib.blueprint2.dom.IDocument;
 
@@ -9,15 +7,17 @@ public class Test1_helloworld {
 
 	public static void main(String[] arg) {
 
-		String docURI = "resource:///test/bp2/swt/Test1_helloworld.xml";
-		IDocument doc = null;
 		try {
-			doc = Blueprint2.getInstance().loadDocument(docURI);
-		} catch (IOException e) {
+
+			String docURI = "resource:///test/bp2/swt/Test1_helloworld.xml";
+			IDocument doc = Blueprint2.getInstance().loadDocument(docURI);
+
+			// Display display = doc.findTargetById("root");
+			// this.mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// Display display = doc.findTargetById("root");
-		// this.mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
