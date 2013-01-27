e468b08ef3f2c85741b5baf88afb09112a3f1be0
xukun <xukun@xukun-Inspiron-1420>
2013-01-27 22:50:53 +0800

    xxx

--- blueprint3/blueprint3-schema/src/ananas/lib/blueprint/schema/xsd/ctrl/XSTCtrl_annotationType.java
+++ blueprint3/blueprint3-schema/src/ananas/lib/blueprint/schema/xsd/ctrl/XSTCtrl_annotationType.java
@@ -1,5 +1,27 @@
 package ananas.lib.blueprint.schema.xsd.ctrl;
 
+import ananas.lib.blueprint.core.dom.BPNode;
+
 public class XSTCtrl_annotationType extends XSTCtrl_openAttrs {
+
+	public boolean appendChild(BPNode node) {
+
+		if (node == null) {
+			return false;
+
+		} else if (node instanceof XSTCtrl_documentationType) {
+			XSTCtrl_documentationType documentation = (XSTCtrl_documentationType) node;
+			this._addDocumentation(documentation);
+
+		} else {
+			return super.appendChild(node);
+		}
+		return true;
+	}
+
+	private void _addDocumentation(XSTCtrl_documentationType documentation) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
--- blueprint3/blueprint3-schema/src/ananas/lib/blueprint/schema/xsd/ctrl/XSTCtrl_schemaType.java
+++ blueprint3/blueprint3-schema/src/ananas/lib/blueprint/schema/xsd/ctrl/XSTCtrl_schemaType.java
@@ -1,5 +1,54 @@
 package ananas.lib.blueprint.schema.xsd.ctrl;
 
+import ananas.lib.blueprint.core.dom.BPNode;
+
 public class XSTCtrl_schemaType extends XSTCtrl_openAttrs {
+
+	public boolean appendChild(BPNode node) {
+
+		if (node == null) {
+			return false;
+
+		} else if (node instanceof XSTCtrl_annotationType) {
+			XSTCtrl_annotationType annotation = (XSTCtrl_annotationType) node;
+			this._addAnnotation(annotation);
+
+		} else if (node instanceof XSTCtrl_element) {
+			XSTCtrl_element element = (XSTCtrl_element) node;
+			this._addElement(element);
+
+		} else if (node instanceof XSTCtrl_simpleType) {
+			XSTCtrl_simpleType stype = (XSTCtrl_simpleType) node;
+			this._addSimpleType(stype);
+
+		} else if (node instanceof XSTCtrl_complexType) {
+			XSTCtrl_complexType ctype = (XSTCtrl_complexType) node;
+			this._addComplexType(ctype);
+
+		} else {
+			return super.appendChild(node);
+		}
+		return true;
+	}
+
+	private void _addComplexType(XSTCtrl_complexType ctype) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void _addSimpleType(XSTCtrl_simpleType stype) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void _addElement(XSTCtrl_element element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void _addAnnotation(XSTCtrl_annotationType anotation) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
