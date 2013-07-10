cb830bd9347d39f31c769ff29e29cc6538a080b0
xukun <xukun@xukun-ubuntu-64>
2013-07-10 15:19:25 +0800

    changed: switch jdk from 1.7 to 1.6

--- blueprint3/blueprint3-core-impl/src/ananas/lib/impl/blueprint3/core/ParserFactoryImpl.java
+++ blueprint3/blueprint3-core-impl/src/ananas/lib/impl/blueprint3/core/ParserFactoryImpl.java
@@ -1,8 +1,10 @@
 package ananas.lib.impl.blueprint3.core;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 import ananas.lib.blueprint3.util.BPXMLReaderFactory;
 
@@ -10,8 +12,17 @@ public class ParserFactoryImpl implements BPXMLReaderFactory {
 
 	@Override
 	public XMLReader newReader() throws SAXException {
-		String cls = "org.apache.xerces.parsers.SAXParser";
-		return XMLReaderFactory.createXMLReader(cls);
+		// String cls = "org.apache.xerces.parsers.SAXParser";
+		// return XMLReaderFactory.createXMLReader(cls);
+
+		try {
+			SAXParserFactory spf = SAXParserFactory.newInstance();
+			return spf.newSAXParser().getXMLReader();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 }
