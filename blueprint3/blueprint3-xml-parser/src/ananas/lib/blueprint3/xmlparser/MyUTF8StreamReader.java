a864ecbfb9b67811b31ddd2c55f0316fa07d6f07
xukun <xukun0217@gmail.com>
2013-07-11 23:16:15 +0800

    fix : support to utf-8 decode in xml parser.

--- blueprint3/blueprint3-xml-parser/src/ananas/lib/blueprint3/xmlparser/MyUTF8StreamReader.java
+++ blueprint3/blueprint3-xml-parser/src/ananas/lib/blueprint3/xmlparser/MyUTF8StreamReader.java
@@ -21,7 +21,7 @@ public class MyUTF8StreamReader extends Reader {
 	public int read(char[] buf, int off, int len) throws IOException {
 
 		if (len > 0) {
-			int ch = this._read();
+			int ch = this._readChar();
 			if (ch >= 0) {
 				buf[off] = (char) ch;
 				return 1;
@@ -36,11 +36,36 @@ public class MyUTF8StreamReader extends Reader {
 
 	@Override
 	public int read() throws IOException {
-		return this._read();
+		return this._readChar();
 	}
 
-	private int _read() throws IOException {
-		return this.mIn.read();
+	private int _readChar() throws IOException {
+		final InputStream in = this.mIn;
+		final int b0 = in.read();
+		if (b0 < 0) {
+			// EOF
+			return -1;
+		}
+		// shift b0{
+		int cnt = 0;
+		int mask = 0x00ff;
+		for (; (b0 | mask) == 0x00ff; cnt++) {
+			mask = mask >> 1;
+		}
+		int ret = (b0 & mask);
+		final int cntAddition = (b0 <= 0x7f) ? 0 : (cnt - 2);
+		// }end
+		for (int i = cntAddition; i > 0; i--) {
+			final int b = in.read();
+			if (b < 0) {
+				// EOF
+				return -1;
+			}
+			// shift b-addition{
+			ret = ((ret << 6) | (b & 0x3f));
+			// }end
+		}
+		return ret;
 	}
 
 }
